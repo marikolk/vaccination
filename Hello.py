5e3eb0f62acc4e03b250989dbdd4ba4c848548f3
marikolk <152090034+marikolk@users.noreply.github.com>
2023-11-30 12:09:20 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
+pip install openpyxl
 import streamlit as st
 from streamlit.logger import get_logger
 
@@ -6,7 +7,7 @@ LOGGER = get_logger(__name__)
 import pandas as pd
 import io
 import requests
-pip install openpyxl 
+ 
 
 # Replace <repository-url> with your GitHub repository URL
 repo_url = "https://github.com/marikolk/Vaccination"
