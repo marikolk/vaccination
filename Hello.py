b3666dfddfbf01dad53b1c989c6c0f95a11bda3d
marikolk <152090034+marikolk@users.noreply.github.com>
2023-11-30 10:49:04 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -19,33 +19,18 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+  
+streamlit run 
+    st.title("Vaccination app")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    user_type = st.radio("Select your user type:", ["Hospital", "Government"])
 
 
-if __name__ == "__main__":
-    run()
+    if user_type == "Hospital":
+        st.title("Hospital Vaccination App")
+    elif user_type == "Government":
+        st.title("Government Vaccination App")
+
+    if __name__ == "__main__":
+        run()
+
