225a7a149b259187644b6d54f3278f158dac3bbb
marikolk <152090034+marikolk@users.noreply.github.com>
2023-11-30 14:33:16 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,7 +7,6 @@ LOGGER = get_logger(__name__)
 import pandas as pd
 import io
 import requests
-import openpyxl
 
 # Replace <repository-url> with your GitHub repository URL
 repo_url = "https://github.com/marikolk/Vaccination"
